c71183ca825611d3c9d1e33000d2ab504bc9725a
brhmv <allahverdiibrahimov2004@gmail.com>
2022-12-22 00:08:25 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int choice = default;
+
+            Console.WriteLine("Insert number: ");
+            double number1 = Convert.ToInt32(Console.ReadLine());
+
+        Label:
+
+            Console.WriteLine("\nInsert operation: ");
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.OemPlus)
+            {
+                // Console.WriteLine("+");
+                choice = 1;
+            }
+            else if (key.Key == ConsoleKey.Oem2)
+            {
+                // Console.WriteLine("/");
+                choice = 4;
+            }
+            else if (key.Key == ConsoleKey.OemMinus)
+            {
+                choice = 2;
+                // Console.WriteLine("-");
+            }
+            else if (key.Key == ConsoleKey.D8)
+            {
+                //  Console.WriteLine("*");
+                choice = 3;
+            }
+            else
+            {
+                goto Label;
+                Console.WriteLine(key.Key);
+            }
+
+            Console.WriteLine("\nInsert number: ");
+            double number2 = Convert.ToInt32(Console.ReadLine());
+
+            double result = default;
+
+            Console.Write("Result: ");
+            switch (choice)
+            {
+                case 0:
+                    break;
+                case 1:
+                    result = number2 + number1;
+                    Console.WriteLine(result);
+                    break;
+                case 2:
+                    result = number1 - number2;
+                    Console.WriteLine(result);
+                    break;
+                case 3:
+                    result = number1 * number2;
+                    Console.WriteLine(result);
+                    break;
+                case 4:
+                    result = number1 / number2;
+                    Console.WriteLine(result);
+                    break;
+            }
+
+            number1 = result;
+            
+            goto Label;
+
+        }
+    }
+}
+
