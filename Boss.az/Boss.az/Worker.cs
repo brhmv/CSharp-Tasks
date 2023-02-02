9f6a9fe92dcc86eae3c4c158e2a6446f53132df4
brhmv <114811800+brhmv@users.noreply.github.com>
2023-02-03 00:26:28 +0400

    Update Worker.cs

--- Boss.az/Boss.az/Worker.cs
+++ Boss.az/Boss.az/Worker.cs
@@ -1,4 +1,7 @@
-﻿
+
+
+using System.Numerics;
+using System.Xml.Linq;
 
 namespace Boss.az
 {
@@ -7,14 +10,45 @@ namespace Boss.az
 
         public CV? CV { get; set; }
 
-        public Worker(string name, string surname, string city, string phone, sbyte age, CV? cv = null)
+        public  Worker(string name, string surname, string city, string phone, sbyte age, CV? cv = null)
             : base(name, surname, age, phone, city)
         {
             CV = cv;
         }
 
+        public static Worker AddWorker()
+        {
+            while (true)
+            {
+                try
+                {
+                    string? name, surname, city, phone;
+                    sbyte age;
+                    Console.Write("Insert your name: ");
+                    name = Console.ReadLine();
 
-        //AddWorker
+                    Console.Write("Insert your Surname: ");
+                    surname = Console.ReadLine();
+
+                    Console.WriteLine("Insert your age:");
+                    age = Convert.ToSByte(Console.ReadLine());
+
+                    Console.Write("Insert your adress(City): ");
+                    city = Console.ReadLine();
+
+                    Console.Write("Insert your phone: ");
+                    phone = Console.ReadLine();
+
+                    return new(name, surname, city, phone, age);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                    continue;
+                }
+            }
+        }
 
         public void Print()
         {
