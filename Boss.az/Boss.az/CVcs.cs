e920aa5355fbcb1637d8e439836b74e661a6b997
brhmv <114811800+brhmv@users.noreply.github.com>
2023-02-03 00:23:58 +0400

    Update CVcs.cs

--- Boss.az/Boss.az/CVcs.cs
+++ Boss.az/Boss.az/CVcs.cs
@@ -1,17 +1,71 @@
-﻿
+
 namespace Boss.az
 {
     enum Level { A1 = 1, A2, B1, B2, C1, C2 }
 
     class Language
     {
-        public string Name { get; set; }
+        #region Properties
+        public string? Name { get; set; }
         public Level languageLevel { get; set; }
+        #endregion
 
         public Language(string name, Level languageLevel)
         {
             Name = name;
             this.languageLevel = languageLevel;
+        }
+
+        public static Language createLanguage()
+        {
+            while (true)
+            {
+                try
+                {
+                    string? name;
+                    Console.WriteLine("Insert language name: ");
+                    name = Console.ReadLine();
+
+                    Level languageLevel;
+                    Console.WriteLine("Insert level of it( 1)A1 2)A2 3)B1 4)B2 5)C1 6)C2)");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    var a = Level.A1;
+
+                    switch (choice)
+                    {
+                        case 1:
+                            languageLevel = Level.A1;
+                            break;
+                        case 2:
+                            languageLevel = Level.A2;
+                            break;
+                        case 3:
+                            languageLevel = Level.B1;
+                            break;
+                        case 4:
+                            languageLevel = Level.B2;
+                            break;
+                        case 5:
+                            languageLevel = Level.C1;
+                            break;
+                        case 6:
+                            languageLevel = Level.C2;
+                            break;
+                        default:
+                            Console.WriteLine("Choose one of them!");
+                            Console.ReadKey();
+                            continue;
+                    }
+                    return new(name, languageLevel);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                    continue;
+                }
+
+            }
         }
 
         public override string ToString() => $"{Name}=>{languageLevel}\n";
@@ -43,6 +97,7 @@ namespace Boss.az
         #endregion
 
         public CV(string? profession, string? universityName, double universityScore, string[]? skills, string[]? companies,
+
         bool differSertificate, string? linkedin, string? gitlink, List<Language>? languages, double experience)
         {
             Profession = profession;
@@ -57,7 +112,72 @@ namespace Boss.az
             Experience = experience;
         }
 
-        //Create Cv
+        public static CV createCv()
+        {
+            while (true)
+            {
+                try
+                {
+                    string? profession, universityname, linkedin, gitlink;
+
+                    double universityScore, experience;
+
+                    bool differSertificate;
+
+                    string[] skills;
+                    string[] companies;
+
+                    Console.Write("Insert your profession: ");
+                    profession = Console.ReadLine();
+
+                    Console.Write("Insert your university name: ");
+                    universityname = Console.ReadLine();
+
+                    Console.Write("Insert your university enterance score: ");
+                    universityScore = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Insert your university enterance score: ");
+                    experience = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Do you have special certificate from university(yes=1 no=0) ?");
+                    differSertificate = Convert.ToBoolean(Console.ReadLine());
+
+                    Console.Write("Insert your linkedin: ");
+                    linkedin = Console.ReadLine();
+
+                    Console.Write("Insert your gitlink: ");
+                    gitlink = Console.ReadLine();
+
+                    Console.WriteLine("Insert your skills (use | for split):");
+                    skills = Console.ReadLine().Split('|');
+
+
+                    Console.WriteLine("Insert comanies which you worked (use | for split):");
+                    companies = Console.ReadLine().Split('|');
+
+                    List<Language> languages = default;
+
+                    int lcount = default;
+
+                    Console.Write("Insert amount of languages:");
+                    lcount = Convert.ToInt32(Console.ReadLine());
+                                         
+                    for (int i = 0; i < lcount; i++)
+                    {
+                        Language l = Language.createLanguage();
+                        languages.Add(l);
+                    }                   
+
+                    return new(profession, universityname, universityScore, skills, companies, differSertificate, linkedin, gitlink, languages, experience);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                    continue;
+                }
+            }
+        }
 
         public void Print()
         {
