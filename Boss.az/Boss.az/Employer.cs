dcc93f3ac1060f089782c119cd936ed50e475703
brhmv <114811800+brhmv@users.noreply.github.com>
2023-02-03 00:24:29 +0400

    Update Employer.cs

--- Boss.az/Boss.az/Employer.cs
+++ Boss.az/Boss.az/Employer.cs
@@ -1,18 +1,59 @@
-﻿
 namespace Boss.az
 {
     internal class Employer : Person
     {
+
         public List<Vacancy> Vacancies { get; set; }
 
-        public Employer(string name, string surname, int age, string phone, string city, List<Vacancy> vacancies) : base(name, surname, age, phone, city)
+        public Employer(string name, string surname, int age, string phone, string city, List<Vacancy>? vacancies = null) : base(name, surname, age, phone, city)
         {
             Vacancies = vacancies;
         }
 
-        //Add Employer
+        public static Employer createEmployer()
+        {
+            while (true)
+            {
+                try
+                {
+                    string? name, surname, city, phone;
+                    sbyte age;
+                 
+                    Console.Write("Insert your name: ");
+                    name = Console.ReadLine();
 
-        //add Vacancy
+                    Console.Write("Insert your Surname: ");
+                    surname = Console.ReadLine();
+
+                    Console.WriteLine("Insert your age:");
+                    age = Convert.ToSByte(Console.ReadLine());
+
+                    Console.Write("Insert your adress(City): ");
+                    city = Console.ReadLine();
+
+                    Console.Write("Insert your phone: ");
+                    phone = Console.ReadLine();
+
+                    return new(name, surname, age, phone, city);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                    continue;
+                }
+            }
+        }
+
+        public void addVacancy(Vacancy vacancy) { Vacancies.Add(vacancy); }
+
+        public void showVacancies()
+        {
+            foreach (var item in Vacancies)
+            {
+                Console.WriteLine(item);
+            }
+        }
 
         public void Print()
         {
