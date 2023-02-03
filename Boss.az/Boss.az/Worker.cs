62050a0cb83faae4cd4633f1cc0b6cd70ae458f7
brhmv <114811800+brhmv@users.noreply.github.com>
2023-02-03 23:30:12 +0400

    Update Worker.cs

--- Boss.az/Boss.az/Worker.cs
+++ Boss.az/Boss.az/Worker.cs
@@ -1,16 +1,10 @@
-
-
-using System.Numerics;
-using System.Xml.Linq;
-
 namespace Boss.az
 {
     internal class Worker : Person
     {
-
         public CV? CV { get; set; }
 
-        public  Worker(string name, string surname, string city, string phone, sbyte age, CV? cv = null)
+        public Worker(string name, string surname, string city, string phone, sbyte age, CV? cv = null)
             : base(name, surname, age, phone, city)
         {
             CV = cv;
