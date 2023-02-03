df17f575e09f933cd0c732944fe997d785cd2ab9
brhmv <114811800+brhmv@users.noreply.github.com>
2023-02-03 11:34:29 +0400

    Update Program.cs

--- Boss.az/Boss.az/Program.cs
+++ Boss.az/Boss.az/Program.cs
@@ -3,5 +3,30 @@ using Boss.az;
 Console.WriteLine("s");
 
 Employer e = new("a", "b", 4, "ds", "d");
-e.addVacancy(Vacancy.createVacancy());
+e.addVacancy(Vacancy.CreateVacancy());
 e.showVacancies();
+
+////Notification class
+//title
+//content
+//user
+//Datetime
+//status(enum)\
+
+
+
+//filter delegate list yeni her filterden sonra
+
+
+
+//sign in
+//vacancies
+//
+
+
+
+
+///apply
+/// accept reject
+/// 
+
